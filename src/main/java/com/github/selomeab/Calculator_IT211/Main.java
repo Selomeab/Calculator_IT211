752a4723208ca54f02d50173551c622643152ec4
Selomea Beyene <selomea.beyene@gmail.com>
2025-03-18 14:05:53 -0700

    Adds initial Calculator code

---
+++ src/main/java/com/github/selomeab/Calculator_IT211/Main.java
@@ -0,0 +1,12 @@
+package com.github.selomeab.Calculator_IT211;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        int sum = calculator.add(5, 3);
+        System.out.println("5 + 3 = " + sum);
+
+    }
+}
+
